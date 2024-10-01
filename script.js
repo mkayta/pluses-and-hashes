f561c208b3060c36b9ce344687182375ee4360c7
Eng-Mohamed <mkayta2023@gmail.com>
2024-10-01 19:15:05 +0300

    I done this Challenge

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Good Luck 💪🏾
+function plusHashCount(str){
+    let countplus=0;
+    let counthash=0;
+    for(let char of str){
+        if(char==='+'){
+            countplus++;
+        }else if(char==='#'){
+            counthash++;
+        }
+    } 
+    return [countplus,counthash]
+
+}
+
+console.log(plusHashCount("###+")) 
+console.log(plusHashCount("##+++#")) 
+console.log(plusHashCount("#+++#+#++#")) 
+console.log(plusHashCount("5667"))
